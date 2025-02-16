72306267330582c9b9402a588167bc9fd90faccd
SanjeevKrishnaR <sanjeev.krishna.menon@gmail.com>
2025-02-16 12:42:10 +0000

    Updated files

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,25 @@
+package com.mycompany.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AppTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testSayHelloEndpoint() {
+        String url = "http://localhost:" + port + "/api/hello";
+        String response = this.restTemplate.getForObject(url, String.class);
+        assertThat(response).isEqualTo("Hello, World!");
+    }
+}
