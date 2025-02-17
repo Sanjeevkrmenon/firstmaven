74ce79626d2f41e46ed13fc7ac4495352a19f225
SanjeevKrishnaR <sanjeev.krishna.menon@gmail.com>
2025-02-17 14:12:45 +0000

    Your commit message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,17 +1,21 @@
 package com.mycompany.app;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/")
 public class App {
-    
-    @GetMapping("/login")
+
+    @GetMapping("/")  // Changed to "/" (root)
     public String sayHello() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/login")
+    public String login() {
         return "Hello, World!";
     }
 
